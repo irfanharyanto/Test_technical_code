497f9fdaa4a461235adbda33dbbf382cd6f1d678
Irfan Haryanto <irfanaat@gmail.com>
2023-02-23 16:49:59 +0700

    Add files via upload

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAngka = new System.Windows.Forms.TextBox();
+            this.btnSegitiga = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.lblHasil = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtAngka
+            // 
+            this.txtAngka.Location = new System.Drawing.Point(49, 46);
+            this.txtAngka.Name = "txtAngka";
+            this.txtAngka.Size = new System.Drawing.Size(184, 22);
+            this.txtAngka.TabIndex = 0;
+            this.txtAngka.Text = " INPUT ANGKA";
+            // 
+            // btnSegitiga
+            // 
+            this.btnSegitiga.Location = new System.Drawing.Point(49, 91);
+            this.btnSegitiga.Name = "btnSegitiga";
+            this.btnSegitiga.Size = new System.Drawing.Size(161, 26);
+            this.btnSegitiga.TabIndex = 1;
+            this.btnSegitiga.Text = "Generate Segitiga";
+            this.btnSegitiga.UseVisualStyleBackColor = true;
+            this.btnSegitiga.Click += new System.EventHandler(this.btnSegitiga_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(216, 91);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 26);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Generate Bilangan Ganjil";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(422, 91);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(191, 26);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Generate Bilangan Prima";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // lblHasil
+            // 
+            this.lblHasil.AutoSize = true;
+            this.lblHasil.Location = new System.Drawing.Point(88, 172);
+            this.lblHasil.Name = "lblHasil";
+            this.lblHasil.Size = new System.Drawing.Size(46, 17);
+            this.lblHasil.TabIndex = 4;
+            this.lblHasil.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(808, 586);
+            this.Controls.Add(this.lblHasil);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnSegitiga);
+            this.Controls.Add(this.txtAngka);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAngka;
+        private System.Windows.Forms.Button btnSegitiga;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label lblHasil;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        int nilaiKali=10;
+        int hasilNilaiKali = 0;
+        int cetakText = 0;
+        int angkaGanjil = 0;
+
+        List<string> listNilai = new List<string>();
+        
+       
+        public Form1()
+        {
+
+            InitializeComponent();
+            
+        }
+
+        private void btnSegitiga_Click(object sender, EventArgs e)
+        {
+            listNilai.Clear();
+
+            var angka = txtAngka.Text.Select(t => int.Parse(t.ToString())).ToArray();
+            string txtHasilNilai = string.Empty.ToString();
+            string txtHasilNilaiFinal = string.Empty.ToString();
+
+            foreach(var angka1 in angka)
+            {
+                string angkaFinal = angka1.ToString();
+                
+
+                if(angka1 > 0 )
+                {
+                    hasilNilaiKali = angka1 * nilaiKali;
+                    txtHasilNilai = hasilNilaiKali.ToString();
+                    txtHasilNilaiFinal = txtHasilNilai;
+
+                } else
+                {
+                    hasilNilaiKali = 10 * nilaiKali;
+                    txtHasilNilai = hasilNilaiKali.ToString();
+                    txtHasilNilaiFinal = txtHasilNilai.Substring(1, txtHasilNilai.Length - 1);
+                }
+
+                listNilai.Add(txtHasilNilaiFinal);
+                nilaiKali = nilaiKali * 10;
+            }
+
+            var text = String.Join(Environment.NewLine, listNilai);
+
+
+            lblHasil.Text = text;
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listNilai.Clear();
+            var angka = txtAngka.Text.Select(t => int.Parse(t.ToString())).ToArray();
+            foreach (var angka1 in angka)
+            {
+                int angkaFinal = angka1;
+                int angkaFinalAkhir = 0;
+               
+
+
+               if(angkaFinal % 2 != 0)
+               {
+                    angkaFinalAkhir = angkaFinal;
+               }
+
+                listNilai.Add(angkaFinalAkhir.ToString());
+            }
+
+            string listString = string.Join(",", listNilai).Replace(",0","");
+
+            lblHasil.Text = listString;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var angka = txtAngka.Text.Select(t => int.Parse(t.ToString())).ToArray();
+            foreach (var angka1 in angka)
+            {
+                string angkaFinal = angka1.ToString();
+
+
+            }
+        }
+    }
+}
